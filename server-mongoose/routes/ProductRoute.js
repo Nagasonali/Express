0082df5d51a996bc9f37a89cb319d577ac74f207
Sonali <nagasonali5676@gmail.com>
2024-11-16 10:45:05 +0530

    Day-18

--- server-mongoose/routes/ProductRoute.js
+++ server-mongoose/routes/ProductRoute.js
@@ -2,13 +2,22 @@ const express = require('express')
 const router = express.Router();
 const Products = require('../models/ProductsModel')
 const validate = require('../config/auth')
+
+router.get('/count', async (req, res) => {
+    try {
+        const count = await Products.countDocuments()
+        return res.status(200).json({ count: count })
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
+})
 // Method : GET  || API : localhost:3000/products/all
 router.get('/all', async (req, res) => {
     try {
         const products = await Products.find()
-        res.status(200).json(products)
+        return res.status(200).json(products)
     } catch (error) {
-        res.status(500).json({ message: error.message })
+        return res.status(500).json({ message: error.message })
     }
 })
 
@@ -18,12 +27,12 @@ router.post('/add', async (req, res) => {
         const newproduct = new Products(req.body)
         const { title, img, price } = newproduct
         if (!title || !img || !price) {
-            res.status(400).json({ message: "All fields required" })
+            return res.status(400).json({ message: "All fields required" })
         }
         await newproduct.save()
-        res.status(200).json(newproduct)
+        return res.status(200).json(newproduct)
     } catch (error) {
-        res.status(500).json({ message: error.message })
+        return res.status(500).json({ message: error.message })
     }
 })
 
@@ -33,12 +42,12 @@ router.put('/edit/:id', async (req, res) => {
         const id = req.params.id
         const existingproduct = await Products.findOne({ _id: id })
         if (!existingproduct) {
-            res.status(404).json({ message: "Product not found" })
+            return res.status(404).json({ message: "Product not found" })
         }
         const updatedproduct = await Products.findByIdAndUpdate(id, req.body, { new: true })
-        res.status(200).json(updatedproduct)
+        return res.status(200).json(updatedproduct)
     } catch (error) {
-        res.status(500).json({ message: error.message })
+        return res.status(500).json({ message: error.message })
     }
 })
 
@@ -51,30 +60,11 @@ router.delete('/delete/:id', async (req, res) => {
             res.status(404).json({ message: "Product not found" })
         }
         await Products.findByIdAndDelete(id)
-        res.status(200).json({ message: "Product Deleted" })
+        return res.status(200).json({ message: "Product Deleted" })
     } catch (error) {
-        res.status(500).json({ message: error.message })
+        return res.status(500).json({ message: error.message })
     }
 })
 
 
 module.exports = router
-
-// 1.GET
-// 2.POST
-// 3.PUT
-// 4.DELETE
-
-// 1.READ
-// 2.CREATE
-// 3.UPDATE
-// 4.DELETE
-
-
-// 200 -> OK
-// 404 -> NOT FOUND
-// 500 -> INTERNAL SERVER ERROR
-// 201 -> CREATED
-// 400 -> BAD Request
-// 401 -> UnAuthorized
-// 409 -> Conflict
